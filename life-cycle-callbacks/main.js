2ab70214e998280c85ce0e0d38276f15d3538dfd
chrisdavidmills <chrisdavidmills@gmail.com>
2017-12-15 16:29:45 -0600

    updating lifeycle callback example

--- life-cycle-callbacks/main.js
+++ life-cycle-callbacks/main.js
@@ -1,31 +1,92 @@
 // Create a class for the element
 class Square extends HTMLElement {
+  // Specify observed attributes so that
+  // attributeChangedCallback will work
+  static get observedAttributes() {return ['w', 'l']; }
+
   constructor() {
     // Always call super first in constructor
     super();
 
-    var width = this.getAttribute('w');
-    var height = this.getAttribute('w');
-    var bgColor = this.getAttribute('c');
-
     var shadow = this.attachShadow({mode: 'open'});
 
     var div = document.createElement('div');
-    shadow.appendChild(div);
-
     var style = document.createElement('style');
-    style.textContent = 'div {' +
-                        ' width: ' + width + 'px;' +
-                        ' height: ' + height + 'px;' +
-                        ' background-color: ' + bgColor;
     shadow.appendChild(style);
-
+    shadow.appendChild(div);
   }
 
   connectedCallback() {
-    console.log('Custom square element registered on page.');
+    console.log('Custom square element added to page.');
+    updateStyle(this);
+  }
+
+  disconnectedCallback() {
+    console.log('Custom square element removed from page.');
+  }
+
+  adoptedCallback() {
+    console.log('Custom square element moved to new page.');
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('Custom square element attributes changed.');
+    updateStyle(this);
   }
 }
 
-// Define the new element
 customElements.define('custom-square', Square);
+
+
+function updateStyle(elem) {
+  var shadow = elem.shadowRoot;
+  var childNodes = shadow.childNodes;
+  for(var i = 0; i < childNodes.length; i++) {
+    if(childNodes[i].nodeName === 'STYLE') {
+      childNodes[i].textContent = 'div {' +
+                          ' width: ' + elem.getAttribute('l') + 'px;' +
+                          ' height: ' + elem.getAttribute('l') + 'px;' +
+                          ' background-color: ' + elem.getAttribute('c');
+    }
+  }
+}
+
+var add = document.querySelector('.add');
+var update = document.querySelector('.update');
+var remove = document.querySelector('.remove');
+var square;
+
+update.disabled = true;
+remove.disabled = true;
+
+function random(min,max) {
+  var random = Math.floor(Math.random()*(max-min+1)+min);
+  return random;
+}
+
+add.onclick = function() {
+  // Create a custom square element
+  square = document.createElement('custom-square');
+  square.setAttribute('l','100');
+  square.setAttribute('c','red');
+  document.body.appendChild(square);
+
+  update.disabled = false;
+  remove.disabled = false;
+  add.disabled = true;
+};
+
+update.onclick = function() {
+  // Randomly update square's attributes
+  square.setAttribute('l',random(50,200));
+  square.setAttribute('c','rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')');
+};
+
+remove.onclick = function() {
+  // Remove the square
+  document.body.removeChild(square);
+
+  update.disabled = true;
+  remove.disabled = true;
+  add.disabled = false;
+};
