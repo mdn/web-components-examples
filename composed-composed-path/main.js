b68b558188c709a3b3657f6f85259820c2882896
chrisdavidmills <chrisdavidmills@gmail.com>
2018-01-31 13:12:12 +0000

    adding example showing behavior of composed and composed path

---
+++ composed-composed-path/main.js
@@ -0,0 +1,31 @@
+customElements.define('open-shadow',
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      let pElem = document.createElement('p');
+      pElem.textContent = this.getAttribute('text');
+
+      let shadowRoot = this.attachShadow({mode: 'open'})
+        .appendChild(pElem);
+
+  }
+});
+
+customElements.define('closed-shadow',
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      let pElem = document.createElement('p');
+      pElem.textContent = this.getAttribute('text');
+
+      let shadowRoot = this.attachShadow({mode: 'closed'})
+        .appendChild(pElem);
+  }
+});
+
+document.querySelector('html').addEventListener('click',function(e) {
+  console.log(e.composed);
+  console.log(e.composedPath());
+}, true);
