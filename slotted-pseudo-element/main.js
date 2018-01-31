1bc39490e4d5d07fe1e5beae8d7aa57d3b56a3aa
chrisdavidmills <chrisdavidmills@gmail.com>
2018-01-31 22:06:07 +0000

    Adding ::slotted pseudo-element example

---
+++ slotted-pseudo-element/main.js
@@ -0,0 +1,20 @@
+customElements.define('person-details',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      let template = document.getElementById('person-template');
+      let templateContent = template.content;
+
+      const shadowRoot = this.attachShadow({mode: 'open'});
+
+      let style = document.createElement('style');
+      style.textContent = 'div { padding: 10px; border: 1px solid gray; width: 200px; margin: 10px; }' +
+                           'h2 { margin: 0 0 10px; }' +
+                           'ul { margin: 0; }' +
+                           'p { margin: 10px 0; }' +
+                           '::slotted(*) { color: gray; font-family: sans-serif; } ';
+
+      shadowRoot.appendChild(style);
+      shadowRoot.appendChild(templateContent.cloneNode(true));
+  }
+})
