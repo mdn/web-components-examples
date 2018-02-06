c584418f00e5f477414d39ae17a5bf5175d4994f
chrisdavidmills <chrisdavidmills@gmail.com>
2018-02-06 16:44:07 +0000

    a few small stylistic changes

--- host-selectors/main.js
+++ host-selectors/main.js
@@ -12,6 +12,7 @@ class ContextSpan extends HTMLElement {
       style.textContent = 'span:hover { text-decoration: underline; }' +
                           ':host-context(h1) { font-style: italic; }' +
                           ':host-context(h1):after { content: " - no links in headers!" }' +
+                          ':host-context(article, aside) { color: gray; }' +
                           ':host(.footer) { color : red; }' +
                           ':host { background: rgba(0,0,0,0.1); padding: 2px 5px; }';
     }
