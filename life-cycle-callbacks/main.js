6c376f8b8fda41f79ba044857620d52ae518b2f5
olivewind <olive@olivewind.com>
2018-05-22 17:17:08 +0800

    add adopted callback example

--- life-cycle-callbacks/main.js
+++ life-cycle-callbacks/main.js
@@ -1,41 +1,45 @@
-// Create a class for the element
-class Square extends HTMLElement {
-  // Specify observed attributes so that
-  // attributeChangedCallback will work
-  static get observedAttributes() {return ['w', 'l']; }
+function squareFactory(win) {
+  // Create a class for the element
+  class Square extends win.HTMLElement {
+    // Specify observed attributes so that
+    // attributeChangedCallback will work
+    static get observedAttributes() { return ['w', 'l']; }
 
-  constructor() {
-    // Always call super first in constructor
-    super();
+    constructor() {
+      // Always call super first in constructor
+      super();
 
-    var shadow = this.attachShadow({mode: 'open'});
+      var shadow = this.attachShadow({ mode: 'open' });
 
-    var div = document.createElement('div');
-    var style = document.createElement('style');
-    shadow.appendChild(style);
-    shadow.appendChild(div);
+      var div = document.createElement('div');
+      var style = document.createElement('style');
+      shadow.appendChild(style);
+      shadow.appendChild(div);
+    }
+
+    connectedCallback() {
+      console.log('Custom square element added to page.');
+      updateStyle(this);
+    }
+
+    disconnectedCallback() {
+      console.log('Custom square element removed from page.');
+    }
+
+    adoptedCallback() {
+      console.log('Custom square element moved to new page.');
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+      console.log('Custom square element attributes changed.');
+      updateStyle(this);
+    }
   }
-
-  connectedCallback() {
-    console.log('Custom square element added to page.');
-    updateStyle(this);
-  }
-
-  disconnectedCallback() {
-    console.log('Custom square element removed from page.');
-  }
-
-  adoptedCallback() {
-    console.log('Custom square element moved to new page.');
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    console.log('Custom square element attributes changed.');
-    updateStyle(this);
-  }
+  return Square;
 }
 
-customElements.define('custom-square', Square);
+
+customElements.define('custom-square', squareFactory(window));
 
 
 function updateStyle(elem) {
@@ -54,6 +58,7 @@ function updateStyle(elem) {
 var add = document.querySelector('.add');
 var update = document.querySelector('.update');
 var remove = document.querySelector('.remove');
+var adopted = document.querySelector('.adopted');
 var square;
 
 update.disabled = true;
@@ -90,3 +95,32 @@ remove.onclick = function() {
   remove.disabled = true;
   add.disabled = false;
 };
+
+adopted.onclick = function() {
+  adopted.disabled = true;
+
+  function createWindow() {
+    let promise = new Promise(resolve => {
+      let f = document.createElement('iframe');
+      f.onload = e => {
+        resolve(f.contentWindow);
+      };
+      document.body.appendChild(f);
+    });
+    return promise;
+  }
+
+  // 1. Create two iframes, w1 and w2.
+  Promise.all([createWindow(), createWindow()])
+    .then(([w1, w2]) => {
+      // 2. Define a custom element in w1.
+      w1.customElements.define('custom-square', squareFactory(w1));
+
+      let square = w1.document.createElement('custom-square');
+      square.setAttribute('l', '100');
+      square.setAttribute('c', 'red');
+
+      // 3. Adopts the custom element into w2 and invokes its adoptedCallback().
+      w2.document.body.appendChild(square);
+    });
+}
