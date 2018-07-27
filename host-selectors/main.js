81902655adb98066b0528b5be9d1516aa2ccc347
Kevin Nagurski <knagurski@gmail.com>
2018-07-28 00:57:54 +0100

    Use template literals and `const` instead of `let`

--- host-selectors/main.js
+++ host-selectors/main.js
@@ -1,20 +1,23 @@
 class ContextSpan extends HTMLElement {
     constructor() {
       super();
-      let style = document.createElement('style');
-      let span = document.createElement('span');
+
+      const style = document.createElement('style');
+      const span = document.createElement('span');
       span.textContent = this.textContent;
 
       const shadowRoot = this.attachShadow({mode: 'open'});
       shadowRoot.appendChild(style);
       shadowRoot.appendChild(span);
 
-      style.textContent = 'span:hover { text-decoration: underline; }' +
-                          ':host-context(h1) { font-style: italic; }' +
-                          ':host-context(h1):after { content: " - no links in headers!" }' +
-                          ':host-context(article, aside) { color: gray; }' +
-                          ':host(.footer) { color : red; }' +
-                          ':host { background: rgba(0,0,0,0.1); padding: 2px 5px; }';
+      style.textContent = `
+        span:hover { text-decoration: underline; }
+        :host-context(h1) { font-style: italic; }
+        :host-context(h1):after { content: " - no links in headers!" }
+        :host-context(article, aside) { color: gray; }
+        :host(.footer) { color : red; }
+        :host { background: rgba(0,0,0,0.1); padding: 2px 5px; }
+      `;
     }
 }
 
