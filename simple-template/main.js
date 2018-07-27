500d8f0e88a0b2561c8d92382a1336fcec0cc3f4
Kevin Nagurski <knagurski@gmail.com>
2018-07-28 00:36:47 +0100

    Use `const` instead of `let`

--- simple-template/main.js
+++ simple-template/main.js
@@ -2,14 +2,18 @@ customElements.define('my-paragraph',
   class extends HTMLElement {
     constructor() {
       super();
-      let template = document.getElementById('my-paragraph');
-      let templateContent = template.content;
 
-      const shadowRoot = this.attachShadow({mode: 'open'})
-        .appendChild(templateContent.cloneNode(true));
+      const template = document.getElementById('my-paragraph');
+      const templateContent = template.content;
+
+      this.attachShadow({mode: 'open'}).appendChild(
+        templateContent.cloneNode(true)
+      );
+    }
   }
-})
+);
 
-let slottedSpan = document.querySelector('my-paragraph span')
+const slottedSpan = document.querySelector('my-paragraph span');
+
 console.log(slottedSpan.assignedSlot);
 console.log(slottedSpan.slot);
