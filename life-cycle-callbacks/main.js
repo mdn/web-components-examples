3bf4d03ce56ce5098709a2693d2e9d92036ee5bc
brunoais <brunoaiss@gmail.com>
2019-04-10 10:50:43 +0100

    lifecycle-callbacks: Simplified updateStyle()
    Instead of all that getting child nodes and then searching to see which one is the <style> tag,
    just use querySelector().

--- life-cycle-callbacks/main.js
+++ life-cycle-callbacks/main.js
@@ -41,19 +41,13 @@ customElements.define('custom-square', Square);
 
 function updateStyle(elem) {
   const shadow = elem.shadowRoot;
-  const childNodes = Array.from(shadow.childNodes);
-  
-  childNodes.forEach(childNode => {
-    if (childNode.nodeName === 'STYLE') {
-      childNode.textContent = `
-        div {
-          width: ${elem.getAttribute('l')}px;
-          height: ${elem.getAttribute('l')}px;
-          background-color: ${elem.getAttribute('c')};
-        }
-      `;
+  shadow.querySelector('style').textContent = `
+    div {
+      width: ${elem.getAttribute('l')}px;
+      height: ${elem.getAttribute('l')}px;
+      background-color: ${elem.getAttribute('c')};
     }
-  });
+  `;
 }
 
 const add = document.querySelector('.add');
