6f815dbff20609f16c867309fec07b21bff29b0d
Alexander CherryTea <Akiyamka@gmail.com>
2025-12-19 09:32:43 +0000

    default prettier formating

--- expanding-list-web-component/main.js
+++ expanding-list-web-component/main.js
@@ -3,31 +3,31 @@ class ExpandingList extends HTMLUListElement {
   connectedCallback() {
     // Get ul and li elements that are a child of this custom ul element
     // li elements can be containers if they have uls within them
-    const uls = this.querySelectorAll('ul');
-    const lis = this.querySelectorAll('li');
+    const uls = this.querySelectorAll("ul");
+    const lis = this.querySelectorAll("li");
 
     // Hide all child uls
     // These lists will be shown when the user clicks a higher level container
     for (const ul of uls) {
-      ul.style.display = 'none';
+      ul.style.display = "none";
     }
 
     // Look through each li element in the ul
     for (const li of lis) {
       // If this li has a ul as a child, decorate it and add a click handler
-      if (li.querySelectorAll('ul').length > 0) {
+      if (li.querySelectorAll("ul").length > 0) {
         // Add an attribute which can be used by the style
         // to show an open or closed icon
-        li.setAttribute('class', 'closed');
+        li.setAttribute("class", "closed");
 
         // Wrap the li element's text in a new span element
         // so we can assign style and event handlers to the span
         const childText = li.childNodes[0];
-        const newSpan = document.createElement('span');
+        const newSpan = document.createElement("span");
 
         // Copy text from li to span, set cursor style
         newSpan.textContent = childText.textContent;
-        newSpan.style.cursor = 'pointer';
+        newSpan.style.cursor = "pointer";
 
         // Add click handler to this span
         const onClick = (e) => {
@@ -35,16 +35,16 @@ class ExpandingList extends HTMLUListElement {
           const nextUl = e.target.nextElementSibling;
 
           // Toggle visible state and update class attribute on ul
-          if (nextUl.style.display === 'block') {
-            nextUl.style.display = 'none';
-            nextUl.parentNode.setAttribute('class', 'closed');
+          if (nextUl.style.display === "block") {
+            nextUl.style.display = "none";
+            nextUl.parentNode.setAttribute("class", "closed");
           } else {
-            nextUl.style.display = 'block';
-            nextUl.parentNode.setAttribute('class', 'open');
+            nextUl.style.display = "block";
+            nextUl.parentNode.setAttribute("class", "open");
           }
         };
 
-        newSpan.addEventListener('click', onClick);
+        newSpan.addEventListener("click", onClick);
 
         // Add the span and remove the bare text node from the li
         childText.parentNode?.insertBefore(newSpan, childText);
@@ -55,4 +55,4 @@ class ExpandingList extends HTMLUListElement {
 }
 
 // Define the new element
-customElements.define('expanding-list', ExpandingList, { extends: 'ul' });
+customElements.define("expanding-list", ExpandingList, { extends: "ul" });
